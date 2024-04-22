1c1c9fedee01a992532d42f3c8b46a187fce3f3f
Einsteinamarjeetk <156225038+Einsteinamarjeetk@users.noreply.github.com>
2024-04-22 06:39:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-api_id = 27803618
+api_id = 9438917
 
-api_hash = "2cdaef9643189f6bd9c7b31a70257356"
+api_hash = "b86192c6db595de8e8863b43648c55da"
 
-bot_token = "7012342197:AAFTFGxXV7vN0oVdopz3s9xM_GluDWltKEo"
+bot_token = "6606738353:AAGLmO0ZG2YaOKy8ni-M4fjjoyAphFGRqjk"
 
